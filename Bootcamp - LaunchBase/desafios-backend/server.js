3293b808346f73eb366a7f20f6d4fe7678ab35c4
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-06-24 10:30:25 -0300

    finalizado o desafio 3-1

--- Bootcamp - LaunchBase/desafios-backend/server.js
+++ Bootcamp - LaunchBase/desafios-backend/server.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjucks.configure("views", {
+    express: server
+})
 
 server.get("/", function(req, res) {
     res.render("about")
+
+    server.use(function(req, res) {
+        res.status(404).render("not-found");
+      });
 })
 
-server.get("/courses", function() {
+server.get("/courses", function(req, res) {
     res.render("courses")
+
+    server.use(function(req, res) {
+        res.status(404).render("not-found");
+      });
 })
 
 server.listen(5000, function() {
