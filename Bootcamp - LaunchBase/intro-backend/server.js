34d867db9e4c74468ffcb7fac123ca9d9846eb87
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-06-24 22:09:04 -0300

    Atualizando a pagina sobre (about)

--- Bootcamp - LaunchBase/intro-backend/server.js
+++ Bootcamp - LaunchBase/intro-backend/server.js
@@ -9,11 +9,24 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false // permite escrita de links em html
 })
 
 server.get("/", function (req, res) {
-    return res.render("about")
+
+    const about = {
+        avatar_url: "https://avatars0.githubusercontent.com/u/52242403?s=460&u=a0abe26c4476d75ce7309e1bb32c3cd66fd8758f&v=4",
+        name: "Gabriel Ribeiro",
+        role: "Estudante de Engenharia de Controle e Automação no CEFET-RJ",
+        description: "Estagiário na ATG (Americas Trading Group) como QA Tester, com ênfase em testes automatizados em C#",
+        links: [
+            { name: "GitHub", url: "https://github.com/gabrieljoseb" },
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/gabrieljoseribeiro" }
+        ]
+    }
+
+    return res.render("about", { about }) // about: about = about
 })
 
 server.get("/portfolio", function (req, res) {
