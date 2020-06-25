c9b74847fbed250d13fb4ca6cc33927fcf1740a9
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-06-25 08:53:50 -0300

    Finalizado o desafio 3-2: Arquivos nunjucks e dados dinamicos

---
+++ Bootcamp - LaunchBase/desafios-backend/data.js
@@ -0,0 +1,39 @@
+module.exports = {
+    tecnologies: [
+        { name: "HTML" },
+        { name: "CSS" },
+        { name: "Javascript" },
+        { name: "Node.js" },
+        { name: "React" },
+        { name: "React Native" }
+    ],
+    courses: [
+        {
+            id: "starter",
+            image_id: "Starter.64c237cc",
+            description: "Torne-se um programador desejado com esses cursos gratuitos",
+            modules: "10 módulos",
+            price: "Free"
+        },
+        {
+            id: "launchbase",
+            image_id: "LaunchBase.08285320",
+            description: "Domine programação do zero e tenha acesso às melhores oportunidades do mercado",
+            modules: "40 módulos",
+            price: "Free"
+        },
+        {
+            id: "gostack",
+            image_id: "GoStack.83a178a0",
+            description: "Treinamento imersivo nas tecnologias mais modernas de desenvolvimento web e mobile",
+            modules: "45 módulos",
+            price: "Free"
+        }
+    ],
+    footer_links: [
+        { name: "GitHub", url: "https://github.com/Rocketseat" },
+        { name: "Instagram", url: "https://www.instagram.com/rocketseat_oficial/" },
+        { name: "Facebook", url: "https://www.facebook.com/rocketseat/" },
+        { name: "LinkedIn", url: "https://www.linkedin.com/school/rocketseat/" }
+    ]
+}
--- Bootcamp - LaunchBase/desafios-backend/server.js
+++ Bootcamp - LaunchBase/desafios-backend/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const data = require('./data')
+
+const courses = data.courses
+const footer_links = data.footer_links
+const tecnologies = data.tecnologies
 
 const server = express()
 
@@ -8,25 +13,26 @@ server.use(express.static('public'))
 server.set('view engine', 'njk')
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
-server.get("/", function(req, res) {
-    res.render("about")
+server.get("/", function (req, res) {
+    res.render("about", {  courses, footer_links, tecnologies })
 
-    server.use(function(req, res) {
+    server.use(function (req, res) {
         res.status(404).render("not-found");
-      });
+    });
 })
 
-server.get("/courses", function(req, res) {
-    res.render("courses")
+server.get("/courses", function (req, res) {
+    res.render("courses", { courses, footer_links, tecnologies })
 
-    server.use(function(req, res) {
+    server.use(function (req, res) {
         res.status(404).render("not-found");
-      });
+    });
 })
 
-server.listen(5000, function() {
+server.listen(5000, function () {
     console.log('server is running')
 })
