3f70092af7f47360b793b3ec1b13f23212f5b376
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-06-24 21:26:33 -0300

    Exportando e importando javascript

---
+++ Bootcamp - LaunchBase/intro-backend/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário animado com JS puro e CSS animation | Mayk Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "AqJKAJ0TKms",
+        title: "Criando um PWA com React Native & Expo Web | Diego Fernandes",
+        duration: "42 min",
+        price: "Free"
+    },
+    {
+        id: "UftSB4DaRU4",
+        title: "Manipulação de DOM com Javascript, do zero, na prática | Mayk Brito",
+        duration: "97 min",
+        price: "Free"
+    },
+    {
+        id: "Vw9BG9fauIE",
+        title: "É a hora de estudar o DENO? | Diego Fernandes",
+        duration: "15 min",
+        price: "Free"
+    },
+    {
+        id: "2iS3SHwft0k",
+        title: "Prisma 2: Automatize o acesso ao banco de dados | Diego Fernandes",
+        duration: "31 min",
+        price: "Free"
+    },
+    {
+        id: "-VQPimwkstM",
+        title: "Menu de subcategorias com recursividade no Javascript | Mayk Brito",
+        duration: "36 min",
+        price: "Free"
+    }
+]
